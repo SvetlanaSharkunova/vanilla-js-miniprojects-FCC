86716443ba6e495ceba702894030c8d884136080
ssharkunova <sharkunova@yandex.ru>
2021-05-11 16:38:21 +0300

    refactor: rewrite variable btn

--- Counter/app.js
+++ Counter/app.js
@@ -4,15 +4,15 @@ const btnContainer = document.querySelector(".btn-container");
 let count = 0;
 
 btnContainer.addEventListener("click", function(e) {
-  let btn = e.target;
+  let btn = e.target.classList;
 
-  if (btn.classList.contains("btnDecrease")) {
+  if (btn.contains("btnDecrease")) {
     count--;    
     countNumber.style.color = "red";
-  } else if (btn.classList.contains("btnReset")) {
+  } else if (btn.contains("btnReset")) {
     count = 0;
     countNumber.style.color = "black";   
-  } else if (btn.classList.contains("btnIncrease")) {
+  } else if (btn.contains("btnIncrease")) {
     count++;
     countNumber.style.color = "green";    
   }
